8a8310738167746e15d2ad7552a2713e110a1ae0
Gustavo Layber <gus.moitinho@outlook.com>
2025-02-14 18:48:14 -0300

    exceptional structure at the main class

--- DesafioControleFluxo/src/Counter.java
+++ DesafioControleFluxo/src/Counter.java
@@ -2,30 +2,35 @@ import java.util.Scanner;
 
 public class Counter {
     public static void main(String[] args) throws Exception {
-        
+
         Scanner terminal = new Scanner(System.in);
 
         System.out.print("Type the first parameter: ");
 
-            int firsParam = terminal.nextInt();
+        int firsParam = terminal.nextInt();
 
         System.out.print("Type the second parameter: ");
 
-            int secParam = terminal.nextInt();
+        int secParam = terminal.nextInt();
 
-        counter(firsParam, secParam);
+        try {
+            counter(firsParam, secParam);
+
+        } catch (InvalidParamsException e) {
+
+            System.out.println("The second param must be bigger than the first one.");
+        }
     }
 
-    static void counter(int firstVal, int finalVal) throws InvalidParamsException 
-    {    
-        boolean isException = firstVal > finalVal ? true : false; 
-        
-        if(isException) 
-            throw new InvalidParamsException();
-        
-        int diference = finalVal - firstVal; 
+    static void counter(int firstVal, int finalVal) throws InvalidParamsException {
+        boolean isException = firstVal > finalVal ? true : false;
 
-        for(int i = 0 ; i <= diference ; i++) {
+        if (isException)
+            throw new InvalidParamsException();
+
+        int diference = finalVal - firstVal;
+
+        for (int i = 0; i <= diference; i++) {
             System.out.println("Printing number " + i);
         }
     }
